6e232975ace7d76becd58f6bc62e2e4119022a9f
Tyler Norman <tnorman600@gmail.com>
2021-11-03 11:40:51 -0600

    deleted to do comments as I will move that functionality to other sources which will be developed in later commits.

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,4 @@
 // TODO: Include packages needed for this application
-const fs = require("fs");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
@@ -67,11 +66,4 @@ const promptUser = () => {
     ]);
 } 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+promptUser()
