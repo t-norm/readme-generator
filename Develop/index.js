0da5c281335dbc4af15bb3edeb3c59653c3f8d88
Tyler Norman <tnorman600@gmail.com>
2021-11-04 14:28:52 -0600

    reworked license section to be more simplistic. Added link to choosealicense.com for license details

--- Develop/index.js
+++ Develop/index.js
@@ -41,9 +41,13 @@ const promptUser = () => {
             name: "license",
             message: "What license should this project use?",
             choices: [
+                "GNU AGPLv3",
+                "GNU GPLv3",
+                "GNU LGPLv3",
+                "Mozilla Public 2.0",
+                "Apache 2.0",
                 "MIT",
-                "ISC",
-                "GNU GPL",
+                "Boost Software 1.0",
                 "Unlicensed"
             ]
         },
